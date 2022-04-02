9e03fc3aa0725f7fb0cd46017fd10eaf4f1c7d61
Kevin Lai <kevinlai@hk01.com>
2022-04-02 16:18:25 +0800

    feat: rewrite export ios native module

--- ios/lib/ReparentableDestination.m
+++ ios/lib/ReparentableDestination.m
@@ -1,7 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTViewManager.h>
 
-@interface RCT_EXTERN_MODULE(RCTPortalDestination, RCTViewManager)
+// @interface RCT_EXTERN_MODULE(RCTPortalDestination, RCTViewManager)
+@interface RCTPortalDestinationManager : RCTViewManager
+@end
+
+@implementation RCTPortalDestinationManager
+
+RCT_EXPORT_MODULE(RCTPortalDestination)
 
 RCT_EXPORT_VIEW_PROPERTY(name, NSString)
 
--- ios/lib/ReparentableOrigin.m
+++ ios/lib/ReparentableOrigin.m
@@ -1,7 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTViewManager.h>
 
-@interface RCT_EXTERN_MODULE(RCTPortalOrigin, RCTViewManager)
+// @interface RCT_EXTERN_MODULE(RCTPortalOrigin, RCTViewManager)
+@interface RCTPortalOriginManager : RCTViewManager
+@end
+
+@implementation RCTPortalOriginManager
+
+RCT_EXPORT_MODULE(RCTPortalOrigin)
 
 RCT_EXPORT_VIEW_PROPERTY(destination, NSString)
 
